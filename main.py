518b712275c7d4b652c1a6fc9b9a69e6ec8f06c4
lozza <lxw@purelxw.me>
2021-06-10 02:30:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import os
 import requests
 import random
-import time
 import string
-import lozzaU, lozzaP
 
 genEmail = True
 providerEmail = ""
 chars = string.ascii_letters + string.digits + ')(*&^%$£"!'
-random.seed = (os.urandom(1024))
+random.seed = (os.urandom(1024))        
 url = 'scammer'
 
 loop = True 
 while (loop == True):
-    names = random.randint(0, len(lozzaU.library)-1)
-    passwords = random.randint(0, len(lozzaP.library)-1)
+    names = random.randint(0, len(chars)-1)
+    passwords = random.randint(0, len(chars)-1)
     check = random.randint(0, 1)
 
     if (genEmail == True):
         globe = random.randint(0, 2)
-        import genEmail
         globe = genEmail.domain(globe)
         providerEmail = globe
 
 name_extra = ''.join(random.choice(string.digits))
 
-username = lozzaU + '@purelxw.me'
+username = chars + '@purelxw.me'
 password = ''.join(random.choice(chars) for i in range(80))
 
 requests.post(url, allow_redirects=False, data={
